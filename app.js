054cca47de65462362697af18cd5814676ba286e
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2021-08-28 22:25:48 -0500

    50% de avance del hero

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger_button = document.querySelector(".header__menu");
 const menu = document.querySelector(".hide-for-mobile");
 const overlay = document.querySelector(".overlay");
 const fadeElements = document.querySelectorAll(".has-fade"); //All element that have has-fade class
+const body = document.querySelector(".body");
 
 hamburger_button.addEventListener("click", openMenu);
 
@@ -12,6 +13,7 @@ function openMenu(){
             element.classList.remove("fade-in");
             element.classList.add("fade-out");
         })
+        body.classList.remove("no-scroll");
         /* overlay.classList.remove("fade-in")
         overlay.classList.add("fade-out") */
     }
@@ -21,6 +23,7 @@ function openMenu(){
             element.classList.add("fade-in");
             element.classList.remove("fade-out");
         })
+        body.classList.add("no-scroll");
         /*overlay.classList.add("fade-in");
         overlay.classList.remove("fade-out"); */
     }
